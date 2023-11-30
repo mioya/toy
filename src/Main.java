b947f27656df0d5d79bbfe67b1c1580ae29a72d5
mio <powalsld@naver.com>
2023-11-30 22:03:02 +0900

    charAt 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-package com.company;
+import algo.Solution1;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
     }
 }
---
+++ src/algo/Solution1.java
@@ -0,0 +1,38 @@
+package algo;
+
+/*
+* Input 1 -> 2 -> 3
+*       8 -> 8 -> 8
+* Output: 1011
+*
+* 문자열처럼 생긴 음이 아닌 정수값 num1, num2
+* 문자열로 num1, num2 합을 return
+* */
+public class Solution1 {
+    public static void main(String[] args) {
+        System.out.println(new Solution1().solve("123", "888"));
+    }
+
+    public String solve(String num1, String num2){
+        int carry =0;
+        StringBuilder result = new StringBuilder();
+        int num1LastIndex = num1.length() - 1; //2
+        int num2LastIndex = num2.length() - 1; //2
+
+        while(num1LastIndex >= 0 || num2LastIndex>=0) {
+            int num1LastNum = num1.charAt(num1LastIndex) - '0';
+            int num2LastNum = num2.charAt(num2LastIndex) - '0';
+
+            int sum = num1LastNum + num2LastNum + carry;
+            carry = sum/10;
+            result.append(sum%10);
+
+            num1LastIndex--;
+            num2LastIndex--;
+        }
+        if(carry>= 0){
+            result.append(carry);
+        }
+        return result.reverse().toString();
+    }
+}
