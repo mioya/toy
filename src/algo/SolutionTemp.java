61626ae3b0890bccacbbc93d2b1aa5117339952b
mio <powalsld@naver.com>
2023-12-01 23:01:33 +0900

    solution 2

---
+++ src/algo/SolutionTemp.java
@@ -0,0 +1,15 @@
+package algo;
+
+/**
+ * Input :
+ * Output :
+ * */
+
+public class SolutionTemp {
+    public static void main(String[] args) {
+        new SolutionTemp().solve();
+    }
+
+    public void solve() {
+    }
+}
